1ceb70d310d36f477c8755c89de9322140b2d471
mshubham0403 <75219721+mshubham0403@users.noreply.github.com>
2023-05-19 16:04:20 +0530

    add authentication route

---
+++ server/routes/checkAuth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+
+const theRes =(req,res,next)=>{
+    try{
+        const token=req.headers.authorization.split(" ")[1]
+        const decoded=jwt.verify(token,process.env.secretkey)
+        req.decoded=decoded
+        next() 
+    }
+    catch(err){
+        return res.status(401).json({
+            status:'fail',
+            message:'auth fail',
+            error:err
+        })
+    }
+    
+}
+
+export default theRes;
